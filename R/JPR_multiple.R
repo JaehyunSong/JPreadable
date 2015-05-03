aa7767f676e4cc87fbcab276ff2e7e194ac7eb92
JaehyunSong <tintstyle@gmail.com>
2015-05-03 20:15:14 +0900

    no message

--- R/JPR_multiple.R
+++ R/JPR_multiple.R
@@ -7,6 +7,7 @@
 #' @usage JPR_multiple(file.df)
 #'
 #' @param file.df information of text file. The data frame must have three columns. The First column is id, the second is description, the third is filename(include path)
+#' @param result.only Show only results without likihood(default is FALSE)
 #' @return data.frame
 #'
 #' @examples
@@ -16,30 +17,44 @@
 NULL
 
 
-JPR_multiple <- function(file.df){
+JPR_multiple <- function(file.df, result.only = FALSE){
   empty.vector <- rep(NA, nrow(file.df))
-  result.df <- data.frame(id = empty.vector,
-                          text  = empty.vector,
-                          max_ns = empty.vector,
-                          max_ns_lik = empty.vector,
-                          max_s2 = empty.vector,
-                          max_s2_lik = empty.vector,
-                          max_s3 = empty.vector,
-                          max_s3_lik = empty.vector,
-                          result = empty.vector)
+  if(result.only == FALSE){
+    result.df <- data.frame(id = empty.vector,
+                            text  = empty.vector,
+                            max_ns = empty.vector,
+                            max_ns_lik = empty.vector,
+                            max_s2 = empty.vector,
+                            max_s2_lik = empty.vector,
+                            max_s3 = empty.vector,
+                            max_s3_lik = empty.vector,
+                            result = empty.vector)
 
-  for(i in 1:nrow(file.df)){
-    temp.result <- JPreadable(as.character(file.df[i, 3]))
+    for(i in 1:nrow(file.df)){
+      temp.result <- JPreadable(as.character(file.df[i, 3]))
 
-    result.df[i, ] <- c(file.df[i, 1],
-                        as.character(file.df[i, 2]),
-                        temp.result[[3]][1],
-                        round(temp.result[[2]][temp.result[[3]][1], 2], 3),
-                        temp.result[[3]][2],
-                        round(temp.result[[2]][temp.result[[3]][2], 3], 3),
-                        temp.result[[3]][3],
-                        round(temp.result[[2]][temp.result[[3]][2], 4], 3),
-                        temp.result[[4]])
+      result.df[i, ] <- c(file.df[i, 1],
+                          as.character(file.df[i, 2]),
+                          temp.result[[3]][1],
+                          round(temp.result[[2]][temp.result[[3]][1], 2], 3),
+                          temp.result[[3]][2],
+                          round(temp.result[[2]][temp.result[[3]][2], 3], 3),
+                          temp.result[[3]][3],
+                          round(temp.result[[2]][temp.result[[3]][2], 4], 3),
+                          temp.result[[4]])
+    }
+  }else{
+    result.df <- data.frame(id = empty.vector,
+                            text  = empty.vector,
+                            result = empty.vector)
+
+    for(i in 1:nrow(file.df)){
+      temp.result <- JPreadable(as.character(file.df[i, 3]))
+
+      result.df[i, ] <- c(file.df[i, 1],
+                          as.character(file.df[i, 2]),
+                          temp.result[[4]])
+    }
   }
 
   return(result.df)
